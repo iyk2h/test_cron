2f251c6f1ac72ead8d712f7b6cda772edfdd553b
youngkyunIm <yee0230@naver.com>
2024-01-29 16:51:18 +0900

    mail test4

--- app/api/cron/mail.js
+++ app/api/cron/mail.js
@@ -1,42 +1,43 @@
 const nodemailer = require("nodemailer");
 
-const { email_service, user, pass } = process.env;
-
-const transporter = nodemailer.createTransport({
-  host: "test-cron-eta.vercel.app",
-  port: 465,
-  service: email_service,
-  secure: true,
-  auth: {
-    user: user,
-    pass: pass,
-  },
-});
-
-await new Promise((resolve, reject) => {
-  // verify connection configuration
-  transporter.verify(function (error, success) {
-    if (error) {
-      console.log(error);
-      reject(error);
-    } else {
-      console.log("Server is ready to take our messages");
-      resolve(success);
-    }
-  });
-});
-
-const mailData = {
-  from: {
-    name: `LagLess`,
-    address: user,
-  },
-  to: "yee0230@gmail.com",
-  subject: `form message`,
-  text: "test message",
-};
-
 export async function sendMail() {
+  const { email_service, user, pass } = process.env;
+
+  const transporter = nodemailer.createTransport({
+    // host: "test-cron-eta.vercel.app",
+    host: "smtp.gmail.com",
+    port: 465,
+    service: email_service,
+    auth: {
+      user: user,
+      pass: pass,
+    },
+    secure: true,
+  });
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
+  const mailData = {
+    from: {
+      name: `LagLess`,
+      address: user,
+    },
+    to: "yee0230@gmail.com",
+    subject: `form message`,
+    text: "test message",
+  };
+
   await new Promise((resolve, reject) => {
     // send mail
     transporter.sendMail(mailData, (err, info) => {
