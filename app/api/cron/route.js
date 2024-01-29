89dae7f815ef89154f5a86b385235d7b0dff2114
youngkyunIm <yee0230@naver.com>
2024-01-29 23:22:35 +0900

    mail test34

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,82 +1,85 @@
 "use server";
 
 import { NextResponse } from "next/server";
+import { sendMail } from "./mails";
 const nodemailer = require("nodemailer");
 
 export async function GET(request) {
-  const { email_service, user, pass } = process.env;
+  // const { email_service, user, pass } = process.env;
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.sendgrid.net",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "apikey",
-      pass: pass,
-    },
-  });
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.sendgrid.net",
+  //   port: 587,
+  //   secure: false,
+  //   auth: {
+  //     user: "apikey",
+  //     pass: pass,
+  //   },
+  // });
 
-  try {
-    // verify connection configuration
-    await new Promise((resolve, reject) => {
-      transporter.verify(function (error, success) {
-        if (error) {
-          console.log(error);
-          reject(error);
-        } else {
-          console.log("Server is ready to take our messages");
-          resolve(success);
-        }
-      });
-    });
+  // try {
+  //   // verify connection configuration
+  //   await new Promise((resolve, reject) => {
+  //     transporter.verify(function (error, success) {
+  //       if (error) {
+  //         console.log(error);
+  //         reject(error);
+  //       } else {
+  //         console.log("Server is ready to take our messages");
+  //         resolve(success);
+  //       }
+  //     });
+  //   });
 
-    const mailData = {
-      from: {
-        name: `LagLess`,
-        address: user,
-      },
-      to: "yee0230@gmail.com",
-      subject: `form message`,
-      text: `test message ${Math.random()}`,
-    };
+  //   const mailData = {
+  //     from: {
+  //       name: `LagLess`,
+  //       address: user,
+  //     },
+  //     to: "yee0230@gmail.com",
+  //     subject: `form message`,
+  //     text: `test message ${Math.random()}`,
+  //   };
 
-    const info = await new Promise((resolve, reject) => {
-      // send mail
-      transporter.sendMail(mailData, (err, info) => {
-        if (err) {
-          console.error(err);
-          reject(err);
-        } else {
-          console.log("Email Sent : ", info);
-          resolve(info);
-        }
-      });
-    });
+  //   const info = await new Promise((resolve, reject) => {
+  //     // send mail
+  //     transporter.sendMail(mailData, (err, info) => {
+  //       if (err) {
+  //         console.error(err);
+  //         reject(err);
+  //       } else {
+  //         console.log("Email Sent : ", info);
+  //         resolve(info);
+  //       }
+  //     });
+  //   });
 
-    const response = NextResponse.json({
-      ok: true,
-      success: Math.random(),
-      result: info,
-    });
+  //   const response = NextResponse.json({
+  //     ok: true,
+  //     success: Math.random(),
+  //     result: info,
+  //   });
 
-    // Ensure response is not cached
-    response.headers.set("Cache-Control", "no-store, must-revalidate");
-    response.headers.append("Pragma", "no-cache");
-    response.headers.append("Expires", "0");
+  //   // Ensure response is not cached
+  //   response.headers.set("Cache-Control", "no-store, must-revalidate");
+  //   response.headers.append("Pragma", "no-cache");
+  //   response.headers.append("Expires", "0");
 
-    return response;
-  } catch (error) {
-    console.error("Error occurred:", error);
+  //   return response;
+  // } catch (error) {
+  //   console.error("Error occurred:", error);
 
-    const response = NextResponse.json({
-      state: "NNNNOOOOOO",
-      test: "test",
-      content: error.message,
-    });
+  //   const response = NextResponse.json({
+  //     state: "NNNNOOOOOO",
+  //     test: "test",
+  //     content: error.message,
+  //   });
 
-    // Ensure response is not cached
-    response.headers.set("Cache-Control", "no-store, must-revalidate");
+  //   // Ensure response is not cached
+  //   response.headers.set("Cache-Control", "no-store, must-revalidate");
 
-    return response;
-  }
+  //   return response;
+  // }
+  sendMail();
+  return NextResponse.json({ ok: true });
 }
