540e6db51af6f7d59c18c79bbaf549a02fe71883
youngkyunIm <yee0230@naver.com>
2024-01-29 17:12:58 +0900

    mail test5

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,7 +1,63 @@
 import { NextResponse } from "next/server";
 import { sendMail } from "./mail";
+const nodemailer = require("nodemailer");
 
 export async function GET() {
-  await sendMail();
-  return NextResponse.json({ ok: true, test: "test", content: "contnet" });
+  // await sendMail();
+
+  const { email_service, user, pass } = process.env;
+
+  const transporter = nodemailer.createTransport({
+    // host: "test-cron-eta.vercel.app",
+    port: 465,
+    service: email_service,
+    auth: {
+      user: user,
+      pass: pass,
+    },
+    secure: true,
+  });
+
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
+  const mailData = {
+    from: {
+      name: `LagLess`,
+      address: user,
+    },
+    to: "yee0230@gmail.com",
+    subject: `form message`,
+    text: "test message",
+  };
+
+  const success = await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailData).then((info, err) => {
+      if (info.response.includes("250")) {
+        resolve(true);
+      }
+      reject(err);
+    });
+  });
+
+  if (!success) {
+    return NextResponse.json({
+      state: "NNNNOOOOOO",
+      test: "test",
+      content: "contnet",
+    });
+  } else {
+    return NextResponse.json({ ok: true, test: "test", content: "contnet" });
+  }
 }
