d7e69970d6dc894d9712f8290ef1cada99e0be44
youngkyunIm <yee0230@naver.com>
2024-01-30 16:33:30 +0900

    mail test69

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -10,27 +10,24 @@ function generateRandomSuccess() {
 export async function GET() {
   try {
     const v = generateRandomSuccess();
-    const infoPromise = await sendEmail(v);
+    const info = await sendEmail(v);
 
-    const response = NextResponse.json(
+    return NextResponse.json(
       {
         ok: true,
         success: v,
-        result: infoPromise,
+        result: info,
       },
       { status: 202 }
     );
-
-    return response;
   } catch (error) {
     console.error("Error occurred:", error);
 
-    const response = NextResponse.json({
+    // Return an error response
+    return NextResponse.json({
       state: "NNNNOOOOOO",
       test: "test",
       content: error.message,
     });
-
-    return response;
   }
 }
