67849fc735712eab0fc3b58ff34122b150ad2697
youngkyunIm <yee0230@naver.com>
2024-01-30 13:43:29 +0900

    mail test50

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -11,7 +11,7 @@ function generateRandomSuccess() {
 export async function GET(request) {
   try {
     // The email sending logic is now in the sendEmail function
-    const info = "await sendEmail()";
+    const info = await sendEmail();
 
     const response = NextResponse.json({
       ok: true,
