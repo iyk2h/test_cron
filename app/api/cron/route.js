4453febf2e8072b324ea5b1360a76b8f3ddd7dc3
youngkyunIm <yee0230@naver.com>
2024-01-30 18:12:48 +0900

    mail test80

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -9,20 +9,17 @@ export async function GET() {
   // Use Promise.then to handle the asynchronous operation
   try {
     const info = await sendEmail(v);
-    const response = NextApiResponse.json(
-      {
-        success: v,
-        result: info,
-      },
-      { status: 202 }
-    );
+    const response = NextApiResponse.status(200).json({
+      success: v,
+      result: info,
+    });
     response.headers.set("Cache-Control", "no-store, must-revalidate");
     response.headers.append("Pragma", "no-cache");
     response.headers.append("Expires", "0");
     return response;
   } catch (error) {
     console.error("Error occurred:", error);
-    return NextApiResponse.json({
+    return NextApiResponse.status(404).json({
       state: "NNNNOOOOOO",
       test: "test",
       content: error.message,
