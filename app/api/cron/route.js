89ae4b8b6e7f1ce6b8adc00dd9cdb737f6a6cbf6
youngkyunIm <yee0230@naver.com>
2024-01-30 11:52:17 +0900

    mail test39

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -4,102 +4,103 @@ import { NextResponse } from "next/server";
 // const nodemailer = require("nodemailer");
 
 export async function GET(request) {
-  // const { email_service, user, pass } = process.env;
-
-  // const transporter = nodemailer.createTransport({
-  //   host: "smtp.sendgrid.net",
-  //   port: 587,
-  //   secure: false,
-  //   auth: {
-  //     user: "apikey",
-  //     pass: pass,
-  //   },
-  // });
-
-  // try {
-  //   // verify connection configuration
-  //   await new Promise((resolve, reject) => {
-  //     transporter.verify(function (error, success) {
-  //       if (error) {
-  //         console.log(error);
-  //         reject(error);
-  //       } else {
-  //         console.log("Server is ready to take our messages");
-  //         resolve(success);
-  //       }
-  //     });
-  //   });
-
-  //   const mailData = {
-  //     from: {
-  //       name: `LagLess`,
-  //       address: user,
-  //     },
-  //     to: "yee0230@gmail.com",
-  //     subject: `form message`,
-  //     text: `test message ${Math.random()}`,
-  //   };
-
-  //   const info = await new Promise((resolve, reject) => {
-  //     // send mail
-  //     transporter.sendMail(mailData, (err, info) => {
-  //       if (err) {
-  //         console.error(err);
-  //         reject(err);
-  //       } else {
-  //         console.log("Email Sent : ", info);
-  //         resolve(info);
-  //       }
-  //     });
-  //   });
-
-  //   const response = NextResponse.json({
-  //     ok: true,
-  //     success: Math.random(),
-  //     result: info,
-  //   });
-
-  //   // Ensure response is not cached
-  //   response.headers.set("Cache-Control", "no-store, must-revalidate");
-  //   response.headers.append("Pragma", "no-cache");
-  //   response.headers.append("Expires", "0");
-
-  //   return response;
-  // } catch (error) {
-  //   console.error("Error occurred:", error);
-
-  //   const response = NextResponse.json({
-  //     state: "NNNNOOOOOO",
-  //     test: "test",
-  //     content: error.message,
-  //   });
-
-  //   // Ensure response is not cached
-  //   response.headers.set("Cache-Control", "no-store, must-revalidate");
-
-  //   return response;
-  // }
-  const sgMail = require("@sendgrid/mail");
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const { email_service, user, pass } = process.env;
-  const msg = {
-    from: user, // Change to your recipient
-    to: "yee0230@gmail.com", // Change to your verified sender
-    subject: "Sending with SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  };
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "apikey",
+      pass: pass,
+    },
+  });
 
   try {
-    const result = await new Promise(async (resolve, reject) => {
-      await sgMail.send(msg);
+    // verify connection configuration
+    await new Promise((resolve, reject) => {
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log("Server is ready to take our messages");
+          resolve(success);
+        }
+      });
     });
-    console.log("sent");
-    return NextResponse.json({ ok: result });
+
+    const mailData = {
+      from: {
+        name: `LagLess`,
+        address: user,
+      },
+      to: "yee0230@gmail.com",
+      subject: `form message`,
+      text: `test message ${Math.random()}`,
+    };
+
+    const info = await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          console.log("Email Sent : ", info);
+          resolve(info);
+        }
+      });
+    });
+
+    const response = NextResponse.json({
+      ok: true,
+      success: Math.random(),
+      result: info,
+    });
+
+    // Ensure response is not cached
+    response.headers.set("Cache-Control", "no-store, must-revalidate");
+    response.headers.append("Pragma", "no-cache");
+    response.headers.append("Expires", "0");
+
+    return response;
   } catch (error) {
     console.error("Error occurred:", error);
-    return NextResponse.json({
-      ok: "error",
+
+    const response = NextResponse.json({
+      state: "NNNNOOOOOO",
+      test: "test",
+      content: error.message,
     });
+
+    // Ensure response is not cached
+    response.headers.set("Cache-Control", "no-store, must-revalidate");
+    response.headers.append("Pragma", "no-cache");
+    response.headers.append("Expires", "0");
+
+    return response;
   }
+  // const sgMail = require("@sendgrid/mail");
+  // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  // const msg = {
+  //   from: user, // Change to your recipient
+  //   to: "yee0230@gmail.com", // Change to your verified sender
+  //   subject: "Sending with SendGrid is Fun",
+  //   text: "and easy to do anywhere, even with Node.js",
+  //   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  // };
+
+  // try {
+  //   const result = await new Promise(async (resolve, reject) => {
+  //     await sgMail.send(msg);
+  //   });
+  //   console.log("sent");
+  //   return NextResponse.json({ ok: result });
+  // } catch (error) {
+  //   console.error("Error occurred:", error);
+  //   return NextResponse.json({
+  //     ok: "error",
+  //   });
+  // }
 }
