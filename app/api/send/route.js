a43bc93e2083a4f3e1a40c8ea034b393f525a81a
youngkyunIm <yee0230@naver.com>
2024-01-30 16:05:44 +0900

    mail test65

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,20 +1,16 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  const randomValue = Math.random();
-
-  const response = NextResponse.json({ random: randomValue });
-
-  // Append a unique query parameter to the URL to force cache miss
-  const currentDate = new Date();
-  const cacheBuster = currentDate.getTime();
-  const updatedUrl = `${response.url}?cache=${cacheBuster}`;
-
-  // Set the updated URL
-  response.headers.set("Cache-Control", "no-store, must-revalidate");
-  response.headers.append("Pragma", "no-cache");
-  response.headers.append("Expires", "0");
-  response.headers.set("Location", updatedUrl);
-
-  return response;
+export function GET(request) {
+  return NextResponse.json(
+    {
+      body: request.body,
+      path: request.nextUrl.pathname,
+      query: request.nextUrl.search,
+      cookies: request.cookies.getAll(),
+      v: Math.random(),
+    },
+    {
+      status: 200,
+    }
+  );
 }
