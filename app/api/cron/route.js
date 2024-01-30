8384b6f80058c3141e0528aa31bd95dbe2dc7d84
youngkyunIm <yee0230@naver.com>
2024-01-30 16:41:49 +0900

    mail test72

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -7,33 +7,30 @@ function generateRandomSuccess() {
   return Math.random();
 }
 
-export function GET() {
+export async function GET() {
   const v = generateRandomSuccess();
 
   // Use Promise.then to handle the asynchronous operation
-  return sendEmail(v)
-    .then((info) => {
-      const response = NextResponse.json(
-        {
-          ok: true,
-          success: v,
-          result: info,
-        },
-        { status: 202 }
-      );
-      response.headers.set("Cache-Control", "no-store, must-revalidate");
-      response.headers.append("Pragma", "no-cache");
-      response.headers.append("Expires", "0");
-      return response;
-    })
-    .catch((error) => {
-      console.error("Error occurred:", error);
-
-      // Return an error response
-      return NextResponse.json({
-        state: "NNNNOOOOOO",
-        test: "test",
-        content: error.message,
-      });
+  try {
+    const info = await sendEmail(v);
+    const response = NextResponse.json(
+      {
+        ok: true,
+        success: v,
+        result: info,
+      },
+      { status: 202 }
+    );
+    response.headers.set("Cache-Control", "no-store, must-revalidate");
+    response.headers.append("Pragma", "no-cache");
+    response.headers.append("Expires", "0");
+    return response;
+  } catch (error) {
+    console.error("Error occurred:", error);
+    return NextResponse.json({
+      state: "NNNNOOOOOO",
+      test: "test",
+      content: error.message,
     });
+  }
 }
