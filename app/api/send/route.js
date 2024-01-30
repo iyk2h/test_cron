d319779b663600a5494a41790e4b8d8c1aff84cd
youngkyunIm <yee0230@naver.com>
2024-01-30 15:28:56 +0900

    mail test62

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ random: Math.random });
+  return NextResponse.json({ random: Math.random() });
 }
