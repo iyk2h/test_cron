29d93c148454ea9fa16b40f5c7f7e10e08e57471
youngkyunIm <yee0230@naver.com>
2024-01-30 22:45:31 +0900

    mail test85

--- app/api/cron/[cron]/mail.js
+++ app/api/cron/[cron]/mail.js
@@ -7,50 +7,28 @@ export async function sendEmail(v) {
   const { NEXT_PUBLIC_EMAIL_SERVICE, NEXT_PUBLIC_USER, NEXT_PUBLIC_PASS } =
     process.env;
 
-  // const transporter = createTransport({
-  //   host: "smtp.gmail.com",
-  //   port: 465,
-  //   secure: true,
-  //   auth: {
-  //     user: NEXT_PUBLIC_USER,
-  //     pass: NEXT_PUBLIC_PASS,
-  //   },
-  // });
+  const transporter = createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: NEXT_PUBLIC_USER,
+      pass: NEXT_PUBLIC_PASS,
+    },
+  });
 
-  // const mailData = {
-  //   from: {
-  //     name: `LagLess`,
-  //     address: NEXT_PUBLIC_USER,
-  //   },
-  //   to: "yee0230@gmail.com",
-  //   subject: `${v} form message`,
-  //   text: `test message ${Math.random()}`,
-  // };
-
-  // return new Promise((resolve, reject) => {
-  //   transporter.sendMail(mailData, (err, info) => {
-  //     if (err) {
-  //       console.error(err);
-  //       reject(err);
-  //     } else {
-  //       console.log("Email Sent : ", info);
-  //       resolve(info);
-  //     }
-  //   });
-  // });
-
-  const sgMail = require("@sendgrid/mail");
-  sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
-  const msg = {
-    from: NEXT_PUBLIC_USER, // Change to your recipient
-    to: "yee0230@gmail.com", // Change to your verified sender
-    subject: `${v} Sending with SendGrid is Fun`,
-    text: "and easy to do anywhere, even with Node.js",
-    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  const mailData = {
+    from: {
+      name: `LagLess`,
+      address: NEXT_PUBLIC_USER,
+    },
+    to: "yee0230@gmail.com",
+    subject: `${v} form message`,
+    text: `test message ${Math.random()}`,
   };
 
-  return sgMail
-    .send(msg)
+  return await transporter
+    .sendMail(mailData)
     .then((info) => {
       console.log("sent: ", info);
       return info; // 이 부분이 추가되어야 합니다.
@@ -59,4 +37,25 @@ export async function sendEmail(v) {
       console.error("Error occurred:", error);
       throw error; // 이 부분이 추가되어야 합니다.
     });
+
+  // const sgMail = require("@sendgrid/mail");
+  // sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+  // const msg = {
+  //   from: NEXT_PUBLIC_USER, // Change to your recipient
+  //   to: "yee0230@gmail.com", // Change to your verified sender
+  //   subject: `${v} Sending with SendGrid is Fun`,
+  //   text: "and easy to do anywhere, even with Node.js",
+  //   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  // };
+
+  // return sgMail
+  //   .send(msg)
+  //   .then((info) => {
+  //     console.log("sent: ", info);
+  //     return info; // 이 부분이 추가되어야 합니다.
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error occurred:", error);
+  //     throw error; // 이 부분이 추가되어야 합니다.
+  //   });
 }
